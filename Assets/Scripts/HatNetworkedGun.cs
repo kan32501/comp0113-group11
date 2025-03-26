3c35ec3112a62266b09317fea4bf79f01de94205
Amanda Kane <mndkane@gmail.com>
2025-03-26 15:55:27 +0000

    updated networking fix

--- Assets/Scripts/HatNetworkedGun.cs
+++ Assets/Scripts/HatNetworkedGun.cs
@@ -9,92 +9,93 @@ public class HatNetworkedGun : MonoBehaviour
     private NetworkContext context;
     private bool isGrabbed = false;
     private bool isFiring = false;
+    private bool isOwner = false;
+
+    private Vector3 lastPosition;
 
     void Start()
     {
         gunBehavior = GetComponent<HatGunBehavior>();
         grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
-        context = NetworkScene.Register(this); // Register w Ubiq
+        context = NetworkScene.Register(this);
 
-        // Initially disable gun shooting until grabbed
         gunBehavior.enabled = false;
 
-        // Subscribe to grab and activation events
         grabInteractable.selectEntered.AddListener(OnGrab);
         grabInteractable.selectExited.AddListener(OnRelease);
         grabInteractable.activated.AddListener(OnActivate);
         grabInteractable.deactivated.AddListener(OnDeactivate);
     }
-    Vector3 lastPosition;
 
-    void Update() // ADDED
+    void Update()
     {
-        // Regularly send the current state to ensure synch
-        context.SendJson(new GunMessage() { isGrabbed = isGrabbed, isFiring = isFiring });
+        // Only the owner sends sync updates
+        if (!isOwner) return;
 
-        if(lastPosition != transform.localPosition && isGrabbed)
+        context.SendJson(new GunMessage()
+        {
+            isGrabbed = isGrabbed,
+            isFiring = isFiring
+        });
+
+        if (lastPosition != transform.localPosition)
         {
             lastPosition = transform.localPosition;
             context.SendJson(new GunMessage()
             {
+                isGrabbed = isGrabbed,
+                isFiring = isFiring,
                 position = transform.localPosition
             });
         }
-
-    
     }
 
     void OnGrab(SelectEnterEventArgs args)
     {
-        // Enable gun behavior when grabbed
         gunBehavior.enabled = true;
         isGrabbed = true;
+        isOwner = true;
+        gunBehavior.SetOwnership(true);
     }
 
     void OnRelease(SelectExitEventArgs args)
     {
-        // Disable gun behavior when released
         gunBehavior.enabled = false;
         isGrabbed = false;
+        isOwner = false;
+        gunBehavior.SetOwnership(false);
     }
 
     void OnActivate(ActivateEventArgs args)
     {
-        // Trigger gun firing when activated 
         gunBehavior.Fire();
         isFiring = true;
     }
 
     void OnDeactivate(DeactivateEventArgs args)
     {
-        // Handle stop firing
         isFiring = false;
     }
 
     public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
-    // Syncs gun state - should i only send messages with a state change?*
     {
+        // Skip if this client is currently the owner
+        if (isOwner) return;
+
         var gunMessage = message.FromJson<GunMessage>();
 
-        // Handle incoming messages to synchronize state
-        if (gunMessage.isGrabbed)
-        {
-            gunBehavior.enabled = gunMessage.isGrabbed;
-            isGrabbed = gunMessage.isGrabbed;
-        }
-        if (gunMessage.isFiring)
-        {
-            isFiring = gunMessage.isFiring;
-            if (isFiring)
-            {
-                gunBehavior.Fire();
-            }
-        }
-
         transform.localPosition = gunMessage.position;
+        lastPosition = gunMessage.position;
 
-        // Make sure the logic in Update doesn't trigger as a result of this message
-        lastPosition = transform.localPosition;
+        gunBehavior.enabled = gunMessage.isGrabbed;
+        isGrabbed = gunMessage.isGrabbed;
+
+        if (gunMessage.isFiring && !isFiring)
+        {
+            gunBehavior.Fire();
+        }
+
+        isFiring = gunMessage.isFiring;
     }
 
     private struct GunMessage
